68cd0f3b607d09eacc9d5bf2c90ea701216a14e2
GernotMaier <gernot.maier@desy.de>
2020-10-27 13:33:05 +0100

    irf table production

--- scripts/IRF.generate_lookup_table_parts.sh
+++ scripts/IRF.generate_lookup_table_parts.sh
@@ -55,33 +55,6 @@ RECID=$6
 SIMTYPE=$7
 PARTICLE_TYPE="gamma"
 
-# Hack to scale sizes based on epochs to approx. correct for drop in reflectivity+gain.
-_sizecallineraw=$(grep "* s " ${VERITAS_EVNDISP_AUX_DIR}/ParameterFiles/MSCW.sizecal.runparameter | grep " ${EPOCH} ")
-EPOCH_LABEL=$(echo "$_sizecallineraw" | awk '{print $3}')
-EPOCH_T1SCALE=$(echo "$_sizecallineraw" | awk '{print $4}')
-EPOCH_T2SCALE=$(echo "$_sizecallineraw" | awk '{print $5}')
-EPOCH_T3SCALE=$(echo "$_sizecallineraw" | awk '{print $6}')
-EPOCH_T4SCALE=$(echo "$_sizecallineraw" | awk '{print $7}')
-
-if ! [ $(echo ${EPOCH_T1SCALE} | awk '$1>0.0 && $1<2.0 {print 1}')==1 ]; then
-    echo "T1 SCALING ${EPOCH_T1SCALE} is invalid"
-    exit 1
-fi
-if ! [ $(echo ${EPOCH_T2SCALE} | awk '$1>0.0 && $1<2.0 {print 1}')==1 ]; then
-    echo "T2 SCALING ${EPOCH_T2SCALE} is invalid"
-    exit 1
-fi
-if ! [ $(echo ${EPOCH_T3SCALE} | awk '$1>0.0 && $1<2.0 {print 1}')==1 ]; then
-    echo "T3 SCALING ${EPOCH_T3SCALE} is invalid"
-    exit 1
-fi
-if ! [ $(echo ${EPOCH_T4SCALE} | awk '$1>0.0 && $1<2.0 {print 1}')==1 ]; then
-    echo "T4 SCALING ${EPOCH_T4SCALE} is invalid"
-    exit 1
-fi
-
-SIZESCALING="$EPOCH_T1SCALE,$EPOCH_T2SCALE,$EPOCH_T3SCALE,$EPOCH_T4SCALE"
-
 # input directory containing evndisp products
 if [[ -n "$VERITAS_IRFPRODUCTION_DIR" ]]; then
     INDIR="$VERITAS_IRFPRODUCTION_DIR/$IRFVERSION/$SIMTYPE/${EPOCH}_ATM${ATM}_${PARTICLE_TYPE}/ze${ZA}deg_offset${WOBBLE}deg_NSB${NOISE}MHz"
@@ -98,14 +71,14 @@ if [[ ! -z $VERITAS_IRFPRODUCTION_DIR ]]; then
     ODIR="$VERITAS_IRFPRODUCTION_DIR/$IRFVERSION/$SIMTYPE/${EPOCH_LABEL}_ATM${ATM}_${PARTICLE_TYPE}/Tables"
 fi
 echo "Output file directory: $ODIR"
-mkdir -p $ODIR
-chmod g+w $ODIR
+mkdir -p "$ODIR"
+chmod g+w "$ODIR"
 
 # run scripts and output are written into this directory
 DATE=`date +"%y%m%d"`
 LOGDIR="$VERITAS_USER_LOG_DIR/$DATE/MSCW.MAKETABLES/$(date +%s | cut -c -8)/"
 echo -e "Log files will be written to:\n $LOGDIR"
-mkdir -p $LOGDIR
+mkdir -p "$LOGDIR"
 
 SUBSCRIPT=$(dirname "$0")"/helper_scripts/IRF.lookup_table_parallel_sub"
 
@@ -113,7 +86,7 @@ SUBSCRIPT=$(dirname "$0")"/helper_scripts/IRF.lookup_table_parallel_sub"
 echo "Processing Zenith = $ZA, Wobble = $WOBBLE, Noise = $NOISE"
 
 FSCRIPT="$LOGDIR/$EPOCH-MK-TBL.$DATE.MC-$SIMTYPE-$ZA-$WOBBLE-$NOISE-$EPOCH-$ATM-$RECID"
-rm -f $FSCRIPT.sh
+rm -f "$FSCRIPT.sh"
 
 sed -e "s|ZENITHANGLE|$ZA|" \
     -e "s|WOBBLEOFFSET|$WOBBLE|" \
@@ -122,20 +95,23 @@ sed -e "s|ZENITHANGLE|$ZA|" \
     -e "s|ATMOSPHERE|$ATM|" \
     -e "s|RECONSTRUCTIONID|$RECID|" \
     -e "s|SIMULATIONTYPE|$SIMTYPE|" \
-    -e "s|SIZESCALING|$SIZESCALING|" \
     -e "s|INPUTDIR|$INDIR|" \
     -e "s|OUTPUTDIR|$ODIR|" $SUBSCRIPT.sh > $FSCRIPT.sh
 
-chmod u+x $FSCRIPT.sh
-echo $FSCRIPT.sh
+chmod u+x "$FSCRIPT.sh"
+echo "$FSCRIPT.sh"
 
 # run locally or on cluster
 SUBC=`$(dirname "$0")/helper_scripts/UTILITY.readSubmissionCommand.sh`
 SUBC=`eval "echo \"$SUBC\""`
+if [[ $SUBC == *"ERROR"* ]]; then
+    echo "$SUBC"
+    exit
+fi
 if [[ $SUBC == *qsub* ]]; then
     $SUBC $FSCRIPT.sh
 elif [[ $SUBC == *parallel* ]]; then
-    echo "$FSCRIPT.sh &> $FSCRIPT.log" >> $LOGDIR/runscripts.dat
+    echo "$FSCRIPT.sh &> $FSCRIPT.log" >> "$LOGDIR/runscripts.dat"
 fi
 
 exit
