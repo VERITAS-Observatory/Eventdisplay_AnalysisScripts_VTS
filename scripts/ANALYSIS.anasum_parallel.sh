6fd6b33ccfb2b851eadd8c002c1ae0c5aa1aeee6
GernotMaier <gernot.maier@desy.de>
2020-11-05 21:51:00 +0100

    v500 sync

--- scripts/ANALYSIS.anasum_parallel.sh
+++ scripts/ANALYSIS.anasum_parallel.sh
@@ -4,12 +4,12 @@
 # qsub parameters
 h_cpu=0:59:00; h_vmem=4000M; tmpdir_size=1G
 
-if [ $# -ne 4 ]; then
+if [ $# -lt 4 ]; then
 # begin help message
 echo "
 ANASUM parallel data analysis: submit jobs from an anasum run list
 
-ANALYSIS.anasum_parallel.sh <anasum run list> <mscw directory> <output directory> <run parameter file>
+ANALYSIS.anasum_parallel.sh <anasum run list> <mscw directory> <output directory> <run parameter file> [radial acceptances]
 
 required parameters:
 
@@ -22,6 +22,10 @@ required parameters:
     <run parameter file>    anasum run parameter file
                             (in \$VERITAS_EVNDISP_AUX_DIR/ParameterFiles/;
                              see ANASUM.runparameter for an example)
+
+optional parameters:
+    [radial acceptance]     0=use external radial acceptance;
+                            1=use run-wise radial acceptance (calculated from each data run)
 
 IMPORTANT! Run ANALYSIS.anasum_combine.sh once all parallel jobs have finished!
 
@@ -40,6 +44,7 @@ FLIST=$1
 INDIR=$2
 ODIR=$3
 RUNP=$4
+[[ "$5" ]] && RACC=$5 || RACC="0"
 
 # Check that run list exists
 if [ ! -f "$FLIST" ]; then
@@ -53,7 +58,6 @@ exec 5>&1
 
 # Check that run parameter file exists
 if [[ ! -e "$RUNP" ]]; then
-    #if [[ "$RUNP" == `basename $RUNP` ]]; then
     RUNP="$VERITAS_EVNDISP_AUX_DIR/ParameterFiles/$RUNP"
 fi
 if [ ! -f "$RUNP" ]; then
@@ -64,23 +68,23 @@ fi
 # directory for run scripts
 DATE=`date +"%y%m%d"`
 LOGDIRTEMP="$VERITAS_USER_LOG_DIR/$DATE/ANASUM.ANADATA"
-mkdir -p $LOGDIRTEMP
+mkdir -p "$LOGDIRTEMP"
 
 # temporary run list
 DATECODE=`date +%Y%m%d`
-TEMPLIST=`basename $FLIST`
+TEMPLIST=`basename "$FLIST"`
 TEMPLIST="$LOGDIRTEMP/$DATECODE.PID$$.$TEMPLIST.tmp"
-rm -f $TEMPLIST
-cat $FLIST | grep "*" >> $TEMPLIST
+rm -f "$TEMPLIST"
+cat "$FLIST" | grep "*" >> "$TEMPLIST"
 
 # output directory
-mkdir -p $ODIR
-ODIRBASE=`basename $ODIR`
+mkdir -p "$ODIR"
+ODIRBASE=`basename "$ODIR"`
 echo "Output directory base name: $ODIRBASE"
 
 # get list of runs
-NLINES=`cat $TEMPLIST | wc -l`
-NRUNS=`cat $TEMPLIST | grep -v "VERSION" | wc -l`
+NLINES=`cat "$TEMPLIST" | wc -l`
+NRUNS=`cat "$TEMPLIST" | grep -v "VERSION" | wc -l`
 echo "Total number of runs to analyse: $NRUNS"
 
 # Job submission script
@@ -91,8 +95,9 @@ TIMETAG=`date +"%s"`
 # loop over all runs
 for ((i=1; i <= $NLINES; i++)); do
     echo
-    LINE=`head -n $i $TEMPLIST | tail -n 1`
-    RUN=`head -n $i $TEMPLIST | tail -n 1 | awk '{print $2}'`
+    VERSION=`cat "$TEMPLIST" | grep VERSION`
+    LINE=`head -n $i "$TEMPLIST" | tail -n 1`
+    RUN=`head -n $i "$TEMPLIST" | tail -n 1 | awk '{print $2}'`
 
     if [[ $RUN != "VERSION" ]]; then
         # output file name
@@ -100,15 +105,16 @@ for ((i=1; i <= $NLINES; i++)); do
 
         # temporary log dir
         LOGDIR="$LOGDIRTEMP/${RUN}"
-        mkdir -p $LOGDIR
+        mkdir -p "$LOGDIR"
 
         # temporary per-run file list
         RUNTEMPLIST="$LOGDIR/qsub_analyse_fileList_${ODIRBASE}_${RUN}_${DATECODE}_PID$$"
         rm -f $RUNTEMPLIST
+        echo "$VERSION" > "$RUNTEMPLIST"
         echo "$LINE" > $RUNTEMPLIST
 
         # prepare run scripts
-        FSCRIPT="$LOGDIR/qsub_analyse-$DATE-RUN$RUN"
+        FSCRIPT="$LOGDIR/qsub_analyse-$DATE-RUN$RUN-$(date +%s)"
         echo "Run script written to $FSCRIPT"
         echo "Temporary run list written to $RUNTEMPLIST"
 
@@ -117,13 +123,18 @@ for ((i=1; i <= $NLINES; i++)); do
             -e "s|OUTDIR|$ODIR|"          \
             -e "s|OUTNAME|$ONAME|"        \
             -e "s|RUNNNNN|$RUN|"          \
-            -e "s|RUNPARAM|$RUNP|" $SUBSCRIPT.sh > $FSCRIPT.sh
+            -e "s|RAAACCC|$RACC|"          \
+            -e "s|RUNPARAM|$RUNP|" "$SUBSCRIPT.sh" > "$FSCRIPT.sh"
 
-        chmod u+x $FSCRIPT.sh
+        chmod u+x "$FSCRIPT.sh"
         
         # run locally or on cluster
         SUBC=`$( dirname "$0" )/helper_scripts/UTILITY.readSubmissionCommand.sh`
         SUBC=`eval "echo \"$SUBC\""`
+        if [[ $SUBC == *"ERROR"* ]]; then
+            echo "$SUBC"
+            exit
+        fi
         if [[ $SUBC == *qsub* ]]; then
             JOBID=`$SUBC $FSCRIPT.sh`
             
@@ -133,14 +144,14 @@ for ((i=1; i <= $NLINES; i++)); do
                 JOBID=$( echo "$JOBID" | grep -oP "Your job [0-9.-:]+" | awk '{ print $3 }' )
             fi
             
-			echo "RUN $RUN JOBID $JOBID"
+	    echo "RUN $RUN JOBID $JOBID"
             echo "RUN $RUN SCRIPT $FSCRIPT.sh"
             if [[ $SUBC != */dev/null* ]] ; then
                 echo "RUN $RUN OLOG $FSCRIPT.sh.o$JOBID"
                 echo "RUN $RUN ELOG $FSCRIPT.sh.e$JOBID"
             fi
         elif [[ $SUBC == *parallel* ]]; then
-            echo "$FSCRIPT.sh &> $FSCRIPT.log" >> $LOGDIRTEMP/runscripts.$TIMETAG.dat
+            echo "$FSCRIPT.sh &> $FSCRIPT.log" >> "$LOGDIRTEMP/runscripts.$TIMETAG.dat"
         elif [[ "$SUBC" == *simple* ]] ; then
 	    "$FSCRIPT.sh" |& tee "$FSCRIPT.log"
 	fi
@@ -149,10 +160,10 @@ done
 
 # Execute all FSCRIPTs locally in parallel
 if [[ $SUBC == *parallel* ]]; then
-    cat $LOGDIRTEMP/runscripts.$TIMETAG.dat | $SUBC
+    cat "$LOGDIRTEMP/runscripts.$TIMETAG.dat" | $SUBC
 fi
 
-rm -f $TEMPLIST
+rm -f "$TEMPLIST"
 
 echo ""
 echo "============================================================================================"
