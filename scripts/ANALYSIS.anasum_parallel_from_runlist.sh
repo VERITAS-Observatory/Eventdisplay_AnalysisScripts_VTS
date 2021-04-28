bfe60f990b893ec3777626f5f1432edfa3e757bc
GernotMaier <gernot.maier@desy.de>
2021-04-28 08:50:43 +0200

    add supersoft cuts; make sure that 61 is selected for redHV IRFs

--- scripts/ANALYSIS.anasum_parallel_from_runlist.sh
+++ scripts/ANALYSIS.anasum_parallel_from_runlist.sh
@@ -81,12 +81,14 @@ SIMTYPE_DEFAULT_V6redHV="CARE_RedHV"
 # cut definitions (note: VX to be replaced later in script)
 if [[ $CUTS = "moderate2tel" ]] || [[ $CUTS = "BDTmoderate2tel" ]]; then
     CUT="NTel2-PointSource-Moderate-TMVA-BDT"
-elif [[ $CUTS = "soft2tel" ]] || [[ $CUTS = "BDTsoft2tel" ]] then
+elif [[ $CUTS = "soft2tel" ]] || [[ $CUTS = "BDTsoft2tel" ]]; then
     CUT="NTel2-PointSource-Soft-TMVA-BDT"
 elif [[ $CUTS = "hard2tel" ]] || [[ $CUTS = "BDThard2tel" ]]; then 
     CUT="NTel2-PointSource-Hard-TMVA-BDT"
-elif [[ $CUTS = "hard3tel" ]] || [[ $CUTS = "BDThard3tel" ]] then
+elif [[ $CUTS = "hard3tel" ]] || [[ $CUTS = "BDThard3tel" ]]; then
     CUT="NTel3-PointSource-Hard-TMVA-BDT"
+elif [[ $CUTS = "supersoft" ]]; then
+    CUT="NTel2-PointSource-SuperSoft"
 elif [[ $CUTS = NTel2ModeratePre ]]; then
     CUT="NTel2-PointSource-Moderate-TMVA-Preselection"
 elif [[ $CUTS = NTel2SoftPre ]]; then
@@ -200,6 +202,10 @@ for RUN in ${RUNS[@]}; do
     # V4 and V5: grisu sims with ATM21/22
     if [[ $EPOCH == *"V4"* ]] || [[ $EPOCH == *"V5"* ]]; then
         ATMO=${ATMO/6/2}
+    fi
+    # V6 redHV only for summer atmospheres
+    if [[ $EPOCH == *"V6"* ]] && [[ $OBSL == "obsLowHV" ]]; then
+       ATMO=${ATMO/62/61}
     fi
     if [[ $SIMTYPE == "DEFAULT" ]]; then
         if [[ $EPOCH == *"V4"* ]]; then
