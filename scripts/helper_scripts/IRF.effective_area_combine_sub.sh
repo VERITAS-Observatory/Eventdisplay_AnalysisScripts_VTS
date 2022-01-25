12e01b5e0246765793846204650151809c6d2bd6
GernotMaier <gernot.maier@desy.de>
2022-01-25 17:01:23 +0100

    add DL3reduced writing to effective area combinations

--- scripts/helper_scripts/IRF.effective_area_combine_sub.sh
+++ scripts/helper_scripts/IRF.effective_area_combine_sub.sh
@@ -18,14 +18,21 @@ rm -f $ODIR/$OFILE.list
 ls -1 $EAFILES > $ODIR/$OFILE.list
 
 # combine effective areas (for DL3)
-$EVNDISPSYS/bin/combineEffectiveAreas "$EAFILES" $ODIR/$OFILE DL3 &> $ODIR/$OFILE.log 
+$EVNDISPSYS/bin/combineEffectiveAreas "$ODIR/$OFILE.list" $ODIR/$OFILE DL3 &> $ODIR/$OFILE.log 
 bzip2 $ODIR/$OFILE.combine.log
 
 # combine effective areas (reduced file size for anasum only)
 ODIRANASUM=${ODIR}_anasum
 mkdir -p $ODIRANASUM
 chmod -R g+w $ODIRANASUM
-$EVNDISPSYS/bin/combineEffectiveAreas "$EAFILES" ${ODIRANASUM}/$OFILE anasum &> ${ODIRANASUM}/$OFILE.log 
+$EVNDISPSYS/bin/combineEffectiveAreas "$ODIR/$OFILE.list" ${ODIRANASUM}/$OFILE anasum &> ${ODIRANASUM}/$OFILE.log 
 bzip2 ${ODIRANASUM}/$OFILE.combine.log
+
+# combine effective areas (reduced file size with TH2F replaced by arrays)
+ODIRDL3array=${ODIR}_DL3array
+mkdir -p $ODIRDL3array
+chmod -R g+w $ODIRDL3array
+$EVNDISPSYS/bin/combineEffectiveAreas "$ODIR/$OFILE.list" ${ODIRDL3array}/$OFILE DL3reduced &> ${ODIRDL3array}/$OFILE.log 
+bzip2 ${ODIRDL3array}/$OFILE.combine.log
 
 exit
