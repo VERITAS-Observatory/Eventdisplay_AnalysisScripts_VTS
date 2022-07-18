82a11060d94ff11c357bcd9ad86ff366ee0aa84b
GernotMaier <gernot.maier@desy.de>
2022-07-18 13:48:23 +0200

    laser toffsets

--- scripts/db_scripts/db_run.sh
+++ scripts/db_scripts/db_run.sh
@@ -26,7 +26,6 @@ OVERWRITE="0"
 NTEL="4"
 
 DBDIR="$VERITAS_USER_DATA_DIR/analysis/Results/v490/dbtext/"
-DBDIR=${DBDIR}/${RUN}
 mkdir -p ${DBDIR}
 
 get_start_time()
@@ -42,7 +41,7 @@ get_start_time()
             done
         fi
         db_start_time="${a[$db_start_time_index]}"
-    done < ${DBDIR}/${RUN}.runinfo
+    done < ${DBDIR}/${RUN}/${RUN}.runinfo
     echo "${db_start_time}"
 }
 
@@ -59,7 +58,7 @@ get_end_time()
             done
         fi
         db_end_time="${a[$db_end_time_index]}"
-    done < ${DBDIR}/${RUN}.runinfo
+    done < ${DBDIR}/${RUN}/${RUN}.runinfo
     echo "${db_end_time}"
 }
 
@@ -69,46 +68,57 @@ get_laser_run()
         if [[ ${a[0]} != "run_id" ]]; then
             LASERRUN=${a[0]}
         fi
-    done < ${DBDIR}/${RUN}.laserrun
+    done < ${DBDIR}/${RUN}/${RUN}.laserrun
     echo ${LASERRUN}
 }
 
 read_runinfo()
 {
-    if [[ ! -e ${DBDIR}/${RUN}.runinfo ]] || [[ ${OVERWRITE} == 1 ]]; then
-        ./db_runinfo.sh ${RUN} > ${DBDIR}/${RUN}.runinfo
+    mkdir -p ${DBDIR}/${RUN}
+    if [[ ! -e ${DBDIR}/${RUN}/${RUN}.runinfo ]] || [[ ${OVERWRITE} == 1 ]]; then
+        ./db_runinfo.sh ${RUN} > ${DBDIR}/${RUN}/${RUN}.runinfo
+        echo "runinfo file (written):  ${DBDIR}/${RUN}/${RUN}.runinfo"
+    else
+        echo "runinfo file (found):  ${DBDIR}/${RUN}/${RUN}.runinfo"
     fi
-    echo "runinfo file:  ${DBDIR}/${RUN}.runinfo"
 }
 
 read_rundqm()
 {
-    if [[ ! -e ${DBDIR}/${RUN}.rundqm ]] || [[ ${OVERWRITE} == 1 ]]; then
-        ./db_rundqm.sh ${RUN} > ${DBDIR}/${RUN}.rundqm
+    mkdir -p ${DBDIR}/${RUN}
+    if [[ ! -e ${DBDIR}/${RUN}/${RUN}.rundqm ]] || [[ ${OVERWRITE} == 1 ]]; then
+        ./db_rundqm.sh ${RUN} > ${DBDIR}/${RUN}/${RUN}.rundqm
+        echo "dqm file (written):  ${DBDIR}/${RUN}/${RUN}.rundqm"
+    else
+        echo "dqm file (found):  ${DBDIR}/${RUN}/${RUN}.rundqm"
     fi
-    echo "dqm file:  ${DBDIR}/${RUN}.rundqm"
 }
 
 read_laser_run_and_dqm()
 {
-    if [[ ! -e ${DBDIR}/${RUN}.laserrun ]] || [[ ${OVERWRITE} == 1 ]]; then
-        ./db_laserrun.sh ${RUN} > ${DBDIR}/${RUN}.laserrun
+    mkdir -p ${DBDIR}/${RUN}
+    if [[ ! -e ${DBDIR}/${RUN}/${RUN}.laserrun ]] || [[ ${OVERWRITE} == 1 ]]; then
+        ./db_laserrun.sh ${RUN} > ${DBDIR}/${RUN}/${RUN}.laserrun
+        echo "laser file (written):  ${DBDIR}/${RUN}/${RUN}.laserrun"
+    else
+        echo "laser file (found):  ${DBDIR}/${RUN}/${RUN}.laserrun"
     fi
-    echo "laser file:  ${DBDIR}/${RUN}.laserrun"
+    LASERRUN=$(get_laser_run)
 
-    while IFS="|" read -ra a; do
-        if [[ ${a[0]} != "run_id" ]]; then
-            if [[ ! -e ${DBDIR}/${RUN}.laserrun ]] || [[ ${OVERWRITE} == 1 ]]; then
-                ./db_rundqm.sh ${a[0]} > ${DBDIR}/${a[0]}.rundqm
-            fi
-            echo "laser dqm file:  ${DBDIR}/${a[0]}.rundqm"
+    if [[ -n ${LASERRUN} ]]; then
+        mkdir -p ${DBDIR}/${LASERRUN}
+        if [[ ! -e ${DBDIR}/${LASERRUN}/${LASERRUN}.rundqm ]] || [[ ${OVERWRITE} == 1 ]]; then
+            ./db_rundqm.sh  ${LASERRUN} > ${DBDIR}/${LASERRUN}/${LASERRUN}.rundqm
+            echo "dqm file (laser, written):  ${DBDIR}/${LASERRUN}/${LASERRUN}.rundqm"
+        else
+            echo "dqm file (laser, found):  ${DBDIR}/${LASERRUN}/${LASERRUN}.rundqm"
         fi
-    done < ${DBDIR}/${RUN}.laserrun
+    fi
 }
 
 read_target()
 {
-    if [[ ! -e ${DBDIR}/${RUN}.target ]] || [[ ${OVERWRITE} == 1 ]]; then
+    if [[ ! -e ${DBDIR}/${RUN}/${RUN}.target ]] || [[ ${OVERWRITE} == 1 ]]; then
         while IFS="|" read -ra a; do
             if [[ ${a[0]} == "run_id" ]]; then
                 for (( j=0; j<${#a[@]}; j++ )); 
@@ -120,49 +130,67 @@ read_target()
                 done
             fi
             source_id="${a[$source_index]}"
-        done < ${DBDIR}/${RUN}.runinfo
-        ./db_targetname.sh "${source_id}" > ${DBDIR}/${RUN}.target
+        done < ${DBDIR}/${RUN}/${RUN}.runinfo
+        ./db_targetname.sh "${source_id}" > ${DBDIR}/${RUN}/${RUN}.target
+        echo "target file (written): ${DBDIR}/${RUN}/${RUN}.target"
+    else
+        echo "target file (found): ${DBDIR}/${RUN}/${RUN}.target"
     fi
-    echo "target file: ${DBDIR}/${RUN}.target"
 }
 
 read_camera_rotation()
 {
-    if [[ ! -e ${DBDIR}/${RUN}.camerarotation ]] || [[ ${OVERWRITE} == 1 ]]; then
-        ./db_camerarotation.sh "$(get_start_time)" "$(get_end_time)" > ${DBDIR}/${RUN}.camerarotation
+    if [[ ! -e ${DBDIR}/${RUN}/${RUN}.camerarotation ]] || [[ ${OVERWRITE} == 1 ]]; then
+        ./db_camerarotation.sh "$(get_start_time)" "$(get_end_time)" > ${DBDIR}/${RUN}/${RUN}.camerarotation
+        echo "camera rotation file (written): ${DBDIR}/${RUN}/${RUN}.camerarotation"
+    else
+        echo "camera rotation file (found): ${DBDIR}/${RUN}/${RUN}.camerarotation"
     fi
-    
-    echo "camera rotation file: ${DBDIR}/${RUN}.camerarotation"
 }
 
 read_pixel_data()
 {
-    if [[ ! -e ${DBDIR}/${RUN}.L1_TriggerInfo ]] || [[ ${OVERWRITE} == 1 ]]; then
-        ./db_L1_TriggerInfo.sh ${RUN} > ${DBDIR}/${RUN}.L1_TriggerInfo
+    if [[ ! -e ${DBDIR}/${RUN}/${RUN}.L1_TriggerInfo ]] || [[ ${OVERWRITE} == 1 ]]; then
+        ./db_L1_TriggerInfo.sh ${RUN} > ${DBDIR}/${RUN}/${RUN}.L1_TriggerInfo
+        echo "L1_TriggerInfo file (written): ${DBDIR}/${RUN}/${RUN}.L1_TriggerInfo"
+    else
+        echo "L1_TriggerInfo file (found): ${DBDIR}/${RUN}/${RUN}.L1_TriggerInfo"
     fi
-    echo "L1_TriggerInfo file: ${DBDIR}/${RUN}.L1_TriggerInfo"
 
     for (( j=0; j<${NTEL}; j++ ));
     do
-        if [[ ! -e ${DBDIR}/${RUN}.FADCsettings_TEL${j} ]] || [[ ${OVERWRITE} == 1 ]]; then
-            ./db_FADCsettings.sh "$(get_start_time)" "$(get_end_time)" ${j} > ${DBDIR}/${RUN}.FADCsettings_TEL${j}
+        if [[ ! -e ${DBDIR}/${RUN}/${RUN}.FADCsettings_TEL${j} ]] || [[ ${OVERWRITE} == 1 ]]; then
+            ./db_FADCsettings.sh "$(get_start_time)" "$(get_end_time)" ${j} > ${DBDIR}/${RUN}/${RUN}.FADCsettings_TEL${j}
+            echo "FADC settings TEL ${j} (written): ${DBDIR}/${RUN}/${RUN}.FADCsettings_TEL${j}"
+        else
+            echo "FADC settings TEL ${j} (found): ${DBDIR}/${RUN}/${RUN}.FADCsettings_TEL${j}"
         fi
-        echo "FADC settings TEL ${j}: ${DBDIR}/${RUN}.FADCsettings_TEL${j}"
-        if [[ ! -e ${DBDIR}/${RUN}.HVsettings_TEL${j} ]] || [[ ${OVERWRITE} == 1 ]]; then
-            ./db_HVsettings.sh "$(get_start_time)" "$(get_end_time)" ${j} > ${DBDIR}/${RUN}.HVsettings_TEL${j}
+        if [[ ! -e ${DBDIR}/${RUN}/${RUN}.HVsettings_TEL${j} ]] || [[ ${OVERWRITE} == 1 ]]; then
+            ./db_HVsettings.sh "$(get_start_time)" "$(get_end_time)" ${j} > ${DBDIR}/${RUN}/${RUN}.HVsettings_TEL${j}
+            echo "HV settings TEL ${j} (written): ${DBDIR}/${RUN}/${RUN}.HVsettings_TEL${j}"
+        else
+            echo "HV settings TEL ${j} (found): ${DBDIR}/${RUN}/${RUN}.HVsettings_TEL${j}"
         fi
-        echo "HV settings TEL ${j}: ${DBDIR}/${RUN}.HVsettings_TEL${j}"
     done
 }
 
 read_flasher_calibration()
 {
+    LASERRUN=$(get_laser_run)
     for (( j=1; j<=${NTEL}; j++ ));
     do
-        if [[ ! -e ${DBDIR}/${RUN}.gain_TEL${j} ]] || [[ ${OVERWRITE} == 1 ]]; then
-            ./db_lasergain.sh ${j} > ${DBDIR}/${RUN}.gain_TEL${j}
+        if [[ ! -e ${DBDIR}/${LASERRUN}/${LASERRUN}.gain_TEL${j} ]] || [[ ${OVERWRITE} == 1 ]]; then
+            ./db_lasergain.sh ${LASERRUN} ${j} > ${DBDIR}/${LASERRUN}/${LASERRUN}.gain_TEL${j}
+            echo "Laser gain TEL ${j} (written): ${DBDIR}/${LASERRUN}/${LASERRUN}.gain_TEL${j}"
+        else
+            echo "Laser gain TEL ${j} (found): ${DBDIR}/${LASERRUN}/${LASERRUN}.gain_TEL${j}"
         fi
-        echo "Laser gain TEL ${j}: ${DBDIR}/${RUN}.gain_TEL${j}"
+        if [[ ! -e ${DBDIR}/${LASERRUN}/${LASERRUN}.toffset_TEL${j} ]] || [[ ${OVERWRITE} == 1 ]]; then
+            ./db_lasertoffset.sh ${LASERRUN} ${j} > ${DBDIR}/${LASERRUN}/${LASERRUN}.toffset_TEL${j}
+            echo "Laser toffset TEL ${j} (written): ${DBDIR}/${LASERRUN}/${LASERRUN}.toffset_TEL${j}"
+        else
+            echo "Laser toffset TEL ${j} (found): ${DBDIR}/${LASERRUN}/${LASERRUN}.toffset_TEL${j}"
+        fi
     done
 }
 
