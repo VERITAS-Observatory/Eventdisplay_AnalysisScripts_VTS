8c1740744177df6638b4603d3860c2b82b23d250
GernotMaier <gernot.maier@desy.de>
2022-11-07 15:50:26 +0100

    use absolute path in condor submit file

--- scripts/helper_scripts/UTILITY.condorSubmission.sh
+++ scripts/helper_scripts/UTILITY.condorSubmission.sh
@@ -10,14 +10,16 @@ UTILITY.condorSubmission.sh [submission script] [memory request] [disk request] 
 exit
 fi
 
-SUBFIL=${1}.condor
+SUBSCRIPT=$(readlink -f ${1})
+SUBFIL=${SUBSCRIPT}.condor
+
 rm -f ${SUBFIL}
-echo "JobBatchName = ${1}" > ${SUBFIL}
-echo "Executable = ${1}" > ${SUBFIL}
-echo "Log = ${1}.\$(Cluster)_\$(Process).log" >> ${SUBFIL}
-echo "Output = ${1}.\$(Cluster)_\$(Process).output" >> ${SUBFIL}
-echo "Error = ${1}.\$(Cluster)_\$(Process).error" >> ${SUBFIL}
-echo "Log = ${1}.\$(Cluster)_\$(Process).log" >> ${SUBFIL}
+echo "JobBatchName = ${SUBSCRIPT}" > ${SUBFIL}
+echo "Executable = ${SUBSCRIPT}" > ${SUBFIL}
+echo "Log = ${SUBSCRIPT}.\$(Cluster)_\$(Process).log" >> ${SUBFIL}
+echo "Output = ${SUBSCRIPT}.\$(Cluster)_\$(Process).output" >> ${SUBFIL}
+echo "Error = ${SUBSCRIPT}.\$(Cluster)_\$(Process).error" >> ${SUBFIL}
+echo "Log = ${SUBSCRIPT}.\$(Cluster)_\$(Process).log" >> ${SUBFIL}
 echo "request_memory = ${2}" >> ${SUBFIL}
 echo "request_disk = ${3}" >> ${SUBFIL}
 echo "getenv = True" >> ${SUBFIL}
