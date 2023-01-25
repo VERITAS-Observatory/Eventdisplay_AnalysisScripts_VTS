d92320a631006d324cd59bf158c011917a054331
steob92 <stephan.o-brien@ucdconnect.ie>
2023-01-25 09:25:52 -0500

    Adding options for slurm/sbatch

--- scripts/ANALYSIS.mscw_energy.sh
+++ scripts/ANALYSIS.mscw_energy.sh
@@ -225,6 +225,8 @@ do
             echo "-------------------------------------------------------------------------------"
             echo
         fi
+    elif [[ $SUBC == *sbatch* ]]; then
+	echo "$SUBC $FSCRIPT.sh"      
     elif [[ $SUBC == *parallel* ]]; then
         echo "$FSCRIPT.sh &> $FSCRIPT.log" >> $LOGDIR/runscripts.$TIMETAG.dat
         echo "RUN $AFILE OLOG $FSCRIPT.log"
