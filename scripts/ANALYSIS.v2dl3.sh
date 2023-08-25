26648bbb0bbe85e1a8515ea90f4b6ac7f0049114
GernotMaier <gernot.maier@desy.de>
2023-08-25 10:56:27 +0200

    fixed log file dir

--- scripts/ANALYSIS.v2dl3.sh
+++ scripts/ANALYSIS.v2dl3.sh
@@ -58,8 +58,8 @@ echo -e "Log files will be written to:\n $LOGDIR"
 rm -f ${LOGIDR}/x* 2>/dev/null
 
 # split run list into smaller run lists
-cp -f ${RLIST} ${LOGDIR}/${RLIST}
-(cd "${LOGDIR}" && split -l 100 "${LOGDIR}/${RLIST}")
+cp -f ${RLIST} ${LOGDIR}/
+(cd "${LOGDIR}" && split -l 100 "${LOGDIR}/$(basename ${RLIST})")
 
 FILELISTS=$(ls ${LOGDIR}/x*)
 NFILELISTS=$(ls ${LOGDIR}/x* | wc -l)
