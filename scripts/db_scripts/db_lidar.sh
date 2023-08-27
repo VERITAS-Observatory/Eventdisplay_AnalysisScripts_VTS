70d1d176919facb2b89a9343543e099aa76c1a9b
GernotMaier <gernot.maier@desy.de>
2023-08-27 12:17:42 +0200

    lidar from db

---
+++ scripts/db_scripts/db_lidar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+
+if [ $# -lt 2 ] || [ "$1" = "-h" ]; then
+echo "
+db_lidar: read lidar data from VTS database
+
+./db_lidar.sh <start date> <stop date>
+
+"
+exit
+fi
+
+STARTDATE="$1"
+ENDDATE="$2"
+
+QUERY="SELECT * FROM tblLIDAR_Info WHERE timestamp >= \"${STARTDATE}\" AND timestamp < \"${ENDDATE}\""
+echo "QUERY"
+$($EVNDISPSCRIPTS/db_scripts/db_mysqldb.sh) -e "USE VERITAS; ${QUERY}"  | sed 's/\t/|/g'
