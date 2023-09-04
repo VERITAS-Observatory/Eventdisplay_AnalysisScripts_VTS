d27f5b2a9f05917e277a3505da4182747545cd79
GernotMaier <gernot.maier@desy.de>
2023-09-04 09:07:41 +0200

    remove ^M from dqm string

--- scripts/db_scripts/db_rundqm.sh
+++ scripts/db_scripts/db_rundqm.sh
@@ -25,4 +25,4 @@ RUNSTART=$1
 [[ "$2" ]] && RUNSTOPP=$2 || RUNSTOPP=$1
 QUERY="SELECT run_id , data_category   , status   , status_reason , tel_cut_mask , usable_duration , time_cut_mask , light_level , vpm_config_mask , authors  , comment from tblRun_Analysis_Comments where run_id>=${RUNSTART} and run_id<=${RUNSTOPP};"
 
-$($EVNDISPSCRIPTS/db_scripts/db_mysqldb.sh) -e "USE VOFFLINE; ${QUERY}" | sed 's/\t/|/g'
+$($EVNDISPSCRIPTS/db_scripts/db_mysqldb.sh) -e "USE VOFFLINE; ${QUERY}" | sed 's/\t/|/g' | sed 's/\r//g'
