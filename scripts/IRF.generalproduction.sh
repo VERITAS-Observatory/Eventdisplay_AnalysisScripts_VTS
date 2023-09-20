edd0bc2313f86c19bf650c1a69436b259b69242c
GernotMaier <gernot.maier@desy.de>
2023-09-20 15:29:08 +0200

    add UV processing

--- scripts/IRF.generalproduction.sh
+++ scripts/IRF.generalproduction.sh
@@ -14,7 +14,7 @@ IRF general production for IRFs for all epochs
 required parameters:
 
     <sim type>              simulation type
-                            (e.g. GRISU, CARE_June2020, CARE_RedHV, CARE_UV)
+                            (e.g. GRISU, CARE_June2020, CARE_RedHV, CARE_UV_2212)
     
     <IRF type>              type of instrument response function to produce
                             (e.g. EVNDISP, MAKETABLES, COMBINETABLES,
@@ -39,15 +39,15 @@ IRFTYPE=$2
 process_irfs()
 {
     EPOCHS=$(cat $4 | sort -u)
-    # EPOCHS=(V6_2019_2020w V6_2020_2021w V6_2021_2022w )
-    # for E in ${EPOCHS[@]};
     for E in $EPOCHS
     do
-        if [[ ${E:(-1)} == "w" ]] && [[ $3 == "62" ]]; then
-            continue
-        fi
-        if [[ ${E:(-1)} == "s" ]] && [[ $3 == "61" ]]; then
-            continue
+        if [[ $2 != "CARE_UV_2212" ]]; then
+            if [[ ${E:(-1)} == "w" ]] && [[ $3 == "62" ]]; then
+                continue
+            fi
+            if [[ ${E:(-1)} == "s" ]] && [[ $3 == "61" ]]; then
+                continue
+            fi
         fi
         echo $E $1 $2 $3
         ./IRF.production.sh $2 $1 $E $3
@@ -57,6 +57,8 @@ process_irfs()
 if [[ ${SIMTYPE} == "CARE_June2020" ]] || [[ ${SIMTYPE} == "CARE_RedHV" ]]; then
     process_irfs ${IRFTYPE} ${SIMTYPE} 61 $VERITAS_EVNDISP_AUX_DIR/IRF_EPOCHS_WINTER.dat
     process_irfs ${IRFTYPE} ${SIMTYPE} 62 $VERITAS_EVNDISP_AUX_DIR/IRF_EPOCHS_SUMMER.dat
+elif [[ ${SIMTYPE} == "CARE_UV_2212" ]]; then
+    process_irfs ${IRFTYPE} ${SIMTYPE} 61 $VERITAS_EVNDISP_AUX_DIR/IRF_EPOCHS_obsfilter.dat
 elif [[ ${SIMTYPE} == "GRISU" ]]; then
     ./IRF.production.sh GRISU ${IRFTYPE} V5 21 0
     ./IRF.production.sh GRISU ${IRFTYPE} V5 22 0
