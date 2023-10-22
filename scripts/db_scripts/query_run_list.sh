90095f0b29ff1bda81ab9d1fd71303a632f4a850
GernotMaier <gernot.maier@desy.de>
2023-10-22 13:10:10 +0200

    improved doc

--- scripts/db_scripts/query_run_list.sh
+++ scripts/db_scripts/query_run_list.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 #
-# extract information from VERITAS database required
+# Extract information from VERITAS database required
 # for evndisp analysis for a list of runs
-#
+
+if [ ! -n "$1" ] || [ "$1" = "-h" ]; then
+echo "
+query_run_list.sh: query DB and write DBTEXT files 
+
+./query_run_list <run list> <DBFITS=TRUE/FALSE (default=FALSE)>
+
+Allow to write DB FITS files at the same time
+
+
+"
+exit
+fi
 
 RUNS=$(cat $1)
+[[ "$2" ]] && DBFITS=$2 || DBFITS="FALSE"
+IDIR="$VERITAS_DATA_DIR/shared/DBTEXT/"
+ODIR="./"
 
 for R in $RUNS
 do
     ./db_run.sh ${R}
+#    if [[ $DBFITS == "TRUE" ]]; then
+#        python ./db_write_fits.py \
+#            --run ${R} \
+#            --input_path ${IDIR} \
+#            --output_path ${ODIR}
+#    fi
 done
+
