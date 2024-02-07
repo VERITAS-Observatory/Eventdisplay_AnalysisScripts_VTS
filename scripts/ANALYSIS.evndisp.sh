907ca598e56bb4a6442b89685b52ed120889a3eb
GernotMaier <gernot.maier@desy.de>
2024-02-07 10:46:53 +0100

    memory requirements

--- scripts/ANALYSIS.evndisp.sh
+++ scripts/ANALYSIS.evndisp.sh
@@ -2,7 +2,7 @@
 # script to run eventdisplay analysis for VTS data
 
 # qsub parameters
-h_cpu=11:59:00; h_vmem=2000M; tmpdir_size=25G
+h_cpu=11:59:00; h_vmem=4000M; tmpdir_size=25G
 
 # EventDisplay version
 EDVERSION=`$EVNDISPSYS/bin/evndisp --version | tr -d .`
