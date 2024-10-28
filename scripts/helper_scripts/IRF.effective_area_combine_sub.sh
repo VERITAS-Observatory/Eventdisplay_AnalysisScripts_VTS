ce98d0957bb559e6694993e1feb00501c9341ce7
GernotMaier <gernot.maier@desy.de>
2024-10-28 13:46:55 +0100

    script simplification

--- scripts/helper_scripts/IRF.effective_area_combine_sub.sh
+++ scripts/helper_scripts/IRF.effective_area_combine_sub.sh
@@ -11,27 +11,23 @@ EAFILES=INPUTFILES
 OFILE=OUTPUTFILE
 ODIR=OUTPUTDIR
 
-# temporary directory
-if [[ -n "$TMPDIR" ]]; then
-    DDIR="$TMPDIR/combineEffAreas"
-else
-    DDIR="/tmp/combineEffAreas"
-fi
-mkdir -p "$DDIR"
-echo "Temporary directory: $DDIR"
+DDIR=$(dirname "$EAFILES" | sort -u)
+OPTODIR="$ODIR"
 
 # explicit binding for apptainers
 if [ -n "$EVNDISP_APPTAINER" ]; then
     APPTAINER_MOUNT=" --bind ${VERITAS_EVNDISP_AUX_DIR}:/opt/VERITAS_EVNDISP_AUX_DIR "
     APPTAINER_MOUNT+=" --bind  ${VERITAS_USER_DATA_DIR}:/opt/VERITAS_USER_DATA_DIR "
     APPTAINER_MOUNT+=" --bind ${ODIR}:/opt/ODIR "
-    APPTAINER_MOUNT+=" --bind ${DDIR}:${DDIR} "
+    APPTAINER_MOUNT+=" --bind ${DDIR}:/opt/DDIR "
     echo "APPTAINER MOUNT: ${APPTAINER_MOUNT}"
     APPTAINER_ENV="--env VERITAS_EVNDISP_AUX_DIR=/opt/VERITAS_EVNDISP_AUX_DIR,VERITAS_USER_DATA_DIR=/opt/VERITAS_USER_DATA_DIR,DDIR=/opt/DDIR,CALDIR=/opt/ODIR,LOGDIR=/opt/ODIR,ODIR=/opt/ODIR"
     EVNDISPSYS="${EVNDISPSYS/--cleanenv/--cleanenv $APPTAINER_ENV $APPTAINER_MOUNT}"
     echo "APPTAINER SYS: $EVNDISPSYS"
     # path used by EVNDISPSYS needs to be set
     CALDIR="/opt/ODIR"
+    DDIR="/opt/DDIR/"
+    OPTODIR="/opt/ODIR/"
 fi
 
 inspect_executables()
@@ -46,14 +42,12 @@ inspect_executables()
 # combine effective areas (reduced file size with TH2F replaced by arrays)
 mkdir -p $ODIR
 chmod -R g+w $ODIR
-cp -v $EAFILES $DDIR/
-ls -1 $DDIR/*.root > $DDIR/$OFILE.list
-echo "Copied $(cat $DDIR/$OFILE.list | wc -l) input files to $DDIR"
-$EVNDISPSYS/bin/combineEffectiveAreas "$DDIR/$OFILE.list" ${DDIR}/$OFILE DL3reduced &> ${ODIR}/$OFILE.log
+ls -1 $EAFILES | sed "s|.*/|$DDIR|" > "$ODIR"/"$OFILE".list
+echo "Found $(cat $ODIR/$OFILE.list | wc -l) input files to merge"
+echo "File list: $ODIR/$OFILE.list"
+
+$EVNDISPSYS/bin/combineEffectiveAreas "$OPTODIR/$OFILE.list" ${OPTODIR}/$OFILE DL3reduced &> ${ODIR}/$OFILE.log
 
 # log files
 echo "$(inspect_executables)" >> "$ODIR/$OFILE.log"
-cp -v "${ODIR}/$OFILE.log" "${DDIR}/$OFILE.log"
-$EVNDISPSYS/bin/logFile effAreaCombineLog "${DDIR}/$OFILE.root" "${DDIR}/$OFILE.log"
-
-mv -v -f "${DDIR}/$OFILE.root" ${ODIR}
+$EVNDISPSYS/bin/logFile effAreaCombineLog "${OPTODIR}/$OFILE.root" "${OPTODIR}/$OFILE.log"
