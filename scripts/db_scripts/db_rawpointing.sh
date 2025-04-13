38fbc9ce0ed62e19ad3da0b9ba3db9d80a5ee157
GernotMaier <gernot.maier@desy.de>
2025-04-13 19:15:07 +0200

    Fix reading of zero times in DB

--- scripts/db_scripts/db_rawpointing.sh
+++ scripts/db_scripts/db_rawpointing.sh
@@ -3,12 +3,12 @@
 
 if [ ! -n "$3" ] || [ "$1" = "-h" ]; then
 echo "
-db_rawpointing: read positioner data from VTS database
+db_rawpointing: read raw positioner data from VTS database
 
 ./db_rawpointing.sh <start date> <stop date> <tel id>
 
 "
-exit
+exit 0
 fi
 
 STARTDATE="$1"
@@ -24,5 +24,9 @@ TIMEEND=${TIMEEND//-/}
 TIMEEND=${TIMEEND// /}
 TIMEEND="${TIMEEND}000"
 
-QUERY="SELECT timestamp, elevation_raw, azimuth_raw, elevation_meas, azimuth_meas, elevation_target, azimuth_target FROM tblPositioner_Telescope${TELID}_Status WHERE timestamp >= ${TIMESTART} AND timestamp <= ${TIMEEND};"
-$($EVNDISPSCRIPTS/db_scripts/db_mysqldb.sh) -e "USE VERITAS; ${QUERY}"  | sed 's/\t/|/g'
+if [[ "${STARTDATE}" == *NULL* ]] || [[ "${TIMEEND}" == *NULL* ]]; then
+    echo ""
+else
+    QUERY="SELECT timestamp, elevation_raw, azimuth_raw, elevation_meas, azimuth_meas, elevation_target, azimuth_target FROM tblPositioner_Telescope${TELID}_Status WHERE timestamp >= ${TIMESTART} AND timestamp <= ${TIMEEND};"
+    $($EVNDISPSCRIPTS/db_scripts/db_mysqldb.sh) -e "USE VERITAS; ${QUERY}"  | sed 's/\t/|/g'
+fi
