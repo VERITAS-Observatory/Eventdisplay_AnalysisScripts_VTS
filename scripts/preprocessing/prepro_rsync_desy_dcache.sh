3adc4b5bc6d33fe131d30de8a4a4afdedcea6d29
GernotMaier <gernot.maier@desy.de>
2025-05-14 20:41:57 +0200

    order

--- scripts/preprocessing/prepro_rsync_desy_dcache.sh
+++ scripts/preprocessing/prepro_rsync_desy_dcache.sh
@@ -5,18 +5,26 @@
 BDIR="/pnfs/ifh.de/acs/veritas/diskonly/processed_data"
 IDIR="$VERITAS_DATA_DIR/shared/"
 
+# DBTEXT
+echo "Syncing DBTEXT"
+rsync -av $IDIR/DBTEXT/* "$BDIR/DBTEXT/"
+
+# DBFITS
+echo "Syncing DBFITS"
+rsync -av $IDIR/DBFITS/* "$BDIR/DBFITS/"
+
 # v491.0
+echo "Syncing DL3 v491.0"
 rsync -av $IDIR/processed_data_v491.0/AP/dl3*.tar.gz "$BDIR/v491.0/"
+echo "Syncing mscw v491.0"
 rsync -av $IDIR/processed_data_v491.0/AP/mscw/* "$BDIR/v491.0/AP/"
 
 # v490.7
+echo "Syncing evndisp v490.7 AP"
 rsync -av $IDIR/processed_data_v490.7/AP/evndisp/ "$BDIR/v490.7/AP/evndisp/"
+echo "Syncing evndisp v490.7 NN"
 rsync -av $IDIR/processed_data_v490.7/NN/evndisp/ "$BDIR/v490.7/NN/evndisp/"
+echo "Syncing DL3 v490.7 AP"
 rsync -av $IDIR/processed_data_v490.7/AP/dl3*.tar.gz "$BDIR/v490.7/DL3/"
+echo "Syncing DL3 v490.7 NN"
 rsync -av $IDIR/processed_data_v490.7/NN/dl3*.tar.gz "$BDIR/v490.7/DL3/"
-
-# DBFITS
-rsync -av $IDIR/DBFITS/* "$BDIR/DBFITS/"
-
-# DBTEXT
-rsync -av $IDIR/DBTEXT/* "$BDIR/DBTEXT/"
