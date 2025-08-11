419e698f67f42808e87bcd74a9bb43baf187c752
GernotMaier <gernot.maier@desy.de>
2025-08-11 09:14:54 +0200

    re-order v491

--- scripts/preprocessing/prepro_rsync_desy_dcache.sh
+++ scripts/preprocessing/prepro_rsync_desy_dcache.sh
@@ -13,12 +13,6 @@ rsync -av $IDIR/DBTEXT/* "$BDIR/DBTEXT/"
 echo "Syncing DBFITS"
 rsync -av $IDIR/DBFITS/* "$BDIR/DBFITS/"
 
-# v491.0
-# echo "Syncing DL3 v491.0"
-# rsync -av $IDIR/processed_data_v491.0/AP/dl3*.tar.gz "$BDIR/v491.0/"
-# echo "Syncing mscw v491.0"
-# rsync -av $IDIR/processed_data_v491.0/AP/mscw/* "$BDIR/v491.0/AP/"
-
 # v490.7
 echo "Syncing evndisp v490.7 AP"
 rsync -av $IDIR/processed_data_v490.7/AP/evndisp/ "$BDIR/v490.7/AP/evndisp/"
@@ -28,3 +22,9 @@ echo "Syncing DL3 v490.7 AP"
 rsync -av $IDIR/processed_data_v490.7/AP/dl3*.tar.gz "$BDIR/v490.7/DL3/"
 echo "Syncing DL3 v490.7 NN"
 rsync -av $IDIR/processed_data_v490.7/NN/dl3*.tar.gz "$BDIR/v490.7/DL3/"
+
+# v491.0
+echo "Syncing DL3 v491.0"
+rsync -av $IDIR/processed_data_v491.0/AP/dl3*.tar.gz "$BDIR/v491.0/"
+echo "Syncing mscw v491.0"
+rsync -av $IDIR/processed_data_v491.0/AP/mscw/* "$BDIR/v491.0/AP/"
