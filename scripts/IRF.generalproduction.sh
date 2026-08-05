da91a878dd12be991403325c110539637ee8e53a
GernotMaier <gernot.maier@desy.de>
2026-08-05 13:49:13 +0200

    copy and paste fix

--- scripts/IRF.generalproduction.sh
+++ scripts/IRF.generalproduction.sh
@@ -11,22 +11,22 @@ IRF production for all epochs
 required parameters:
 
     <sim type>              simulation type
-                            Main types: GRISU, CARE_24_20, CARE_RedHV_Feb2024, CARE_UV_2212
-                            CARE_RedHV_Feb2024, CARE_202404, CARE_24_20)
-                            V6 basic types: CARE_202404, CARE_RedHV_Feb2024
+                            Main types: GRISU  CARE_24_20  CARE_RedHV_Feb2024  CARE_UV_2212
+                            CARE_RedHV_Feb2024  CARE_202404  CARE_24_20)
+                            V6 basic types: CARE_202404  CARE_RedHV_Feb2024
 
     <IRF type>              type of instrument response function to produce.
-                            EVNDISP,
-                            MAKETABLES, COMBINETABLES,
-                            TRAINMVANGRES,
-                            TRAINXGBANGRES, ANAXGBANGRES,
-                            TRAINXGBGH, ANAXGBGH,
-                            ANALYSETABLES, ANALYSETABLESXGBTRAIN
-                            PRESELECTEFFECTIVEAREAS, COMBINEPRESELECTEFFECTIVEAREAS,
-                            TRAINTMVA, OPTIMIZETMVA,
-                            ANATABLESEFFAREAS,
-                            EFFECTIVEAREAS, COMBINEEFFECTIVEAREAS,
-                            (EVNDISPCOMPRESS, MVAEVNDISP)
+                            EVNDISP
+                            MAKETABLES  COMBINETABLES
+                            TRAINMVANGRES
+                            TRAINXGBANGRES  ANAXGBANGRES
+                            TRAINXGBGH  ANAXGBGH
+                            ANALYSETABLES  ANALYSETABLESXGBTRAIN
+                            PRESELECTEFFECTIVEAREAS  COMBINEPRESELECTEFFECTIVEAREAS
+                            TRAINTMVA  OPTIMIZETMVA
+                            ANATABLESEFFAREAS
+                            EFFECTIVEAREAS  COMBINEEFFECTIVEAREAS
+                            (EVNDISPCOMPRESS  MVAEVNDISP)
 
 --------------------------------------------------------------------------------
 "
