73f028b482a78fdcd31d3a2cd374859eecd13ada
Gernot Maier <gernot.maier@desy.de>
2026-08-22 12:11:44 +0200

    revert atmo change

--- scripts/helper_scripts/ANALYSIS.anasum_sub.sh
+++ scripts/helper_scripts/ANALYSIS.anasum_sub.sh
@@ -76,24 +76,6 @@ fi
 rm -f $OUTPUTLOGFILE
 touch $OUTPUTLOGFILE
 
-prepare_atmo_string()
-{
-    ATMO=$1
-    EPOCH=$2
-    OBSL=$3
-
-    # V4 and V5 use GrISU IRFs with atmosphere IDs 21/22.
-    if [[ $EPOCH == *"V4"* ]] || [[ $EPOCH == *"V5"* ]]; then
-        ATMO=${ATMO/6/2}
-    fi
-    # V6 UV IRFs are available only for ATM61.
-    if [[ $EPOCH == *"V6"* ]] && [[ $OBSL == "obsFilter" ]]; then
-        ATMO=${ATMO/62/61}
-    fi
-
-    echo "$ATMO"
-}
-
 inspect_executables()
 {
     if [ -n "$EVNDISP_APPTAINER" ]; then
@@ -147,7 +129,6 @@ if [[ $FLIST == "NOTDEFINED" ]]; then
     ATMO=${FORCEDATMO:-$(echo "$RUNINFO" | awk '{print $(3)}')}
     OBSL=$(echo "$RUNINFO" | awk '{print $4}')
     TELTOANA=$(echo "$RUNINFO" | awk '{print "T"$(5)}')
-    ATMO=$(prepare_atmo_string "$ATMO" "$EPOCH" "$OBSL")
 
     REPLACESIMTYPEEff=$(prepare_irf_string "$EPOCH" "$OBSL" $SIMTYPE 0)
     REPLACESIMTYPERad=$(prepare_irf_string "$EPOCH" "$OBSL" $SIMTYPE 1)
